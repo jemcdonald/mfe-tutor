cb5de7db4a29a81a05f95835ccf074ea7d5957a3
Jonathan McDonald <jonathanmcdonald@Jonathans-Work-MacBook-Pro.local>
2023-04-17 14:29:10 -0500

    Initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,22 @@
+console.log("hi there");
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+
+const mount = (el) => {
+    ReactDOM.render(
+        <App />,
+        el
+    );
+};
+
+if (process.env.NODE_ENV === "development") {
+    const devRoot = document.querySelector("#_marketing-dev-root");
+
+    if (devRoot) {
+        mount(devRoot);
+    }
+}
+
+export {mount};
