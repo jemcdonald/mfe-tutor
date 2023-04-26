025e33bfbd1ed489d215e338b60c10d84c329dd0
Jonathan McDonald <jonathanmcdonald@Jonathans-Work-MacBook-Pro.local>
2023-04-26 07:17:56 -0500

    new header

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -1,15 +1,17 @@
 import React from "react";
-// import { mount } from "marketing/MarketingApp";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 // console.log(mount);
 
 export default () => {
     return (
-        <div>
-            <h1>Hello Again!!! Can't contain it.</h1>
-            <hr />
-            <MarketingApp />
-        </div>
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
     )
 };
